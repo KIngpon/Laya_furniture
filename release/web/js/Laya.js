f833646458cf114ead6054cc3f7f4d04d4546c95
aboc <abocd@qq.com>
2018-08-07 18:16:09 +0800

    release

---
+++ release/web/js/Laya.js
@@ -0,0 +1,127 @@
+var WebGL = laya.webgl.WebGL;
+Laya.init(750, 1334, WebGL);
+Laya.stage.bgColor = "#fff";
+Laya.stage.scaleMode = "exactfit";
+
+onStart();
+
+
+
+function onStart(){
+    this.start = new ui.startPageUI();
+    this.startx = this.start.start.x;
+    this.start.start.x = this.start.start.width * -1;
+    Laya.loader.load( ["res/atlas/home/0-tab.atlas",
+"res/atlas/home/0-home.atlas",
+"res/atlas/home/1-floor.atlas",
+"res/atlas/home/2-wallpaper.atlas",
+"res/atlas/home/3-sofa.atlas",
+"res/atlas/home/4-cabinet.atlas",
+"res/atlas/home/5-window.atlas",
+"res/atlas/home/top.atlas",
+"res/atlas/comp.atlas"],null,Laya.Handler.create(this,onProgress,null,false));
+    this.start.y = 0;
+    this.start.width = Laya.Browser.width;
+    Laya.stage.addChild(this.start);
+    this.start.start.on(Laya.Event.CLICK,this,onStartClick);
+}
+
+function onProgress(d){
+    this.start.percent.text = "内容加载中..."+(d * 100).toFixed(2)+"%";
+    // console.info(d)
+    if(d ==1){
+        this.start.percent.visible = false;
+        this.start.start.x = this.startx;
+        this.start.start.loadImage(this.start.start.skin)
+    }
+}
+
+function onStartClick(){
+    console.info(777)
+    onMain();
+    onTab();
+    Laya.stage.removeChildAt(0);
+    // console.info(Laya.stage);
+}
+
+function onMain(){
+    this.main = new ui.mainPageUI();
+    this.main.y = 0;
+    this.main.width = Laya.Browser.width;
+    this.main.wall.loadImage("home/top/wall_bg.png",0,0,this.main.wall.width,this.main.wall.height);
+    this.main.floor.loadImage("home/top/floot_bg.png",0,0,this.main.floor.width,this.main.floor.height);
+    Laya.stage.addChild(this.main);    
+}
+
+
+
+function onTab(){
+    this.ts = new ui.tabSelectUI();
+    this.ts.y = 885-105;
+    this.ts.width = Laya.Browser.width;
+    Laya.stage.addChild(ts);
+    // console.info(this.ts.width ,Laya.Browser.width,Laya.Browser.clientWidth,Laya.stage.scaleX)
+    this.ts.preview.on(Laya.Event.CLICK,this,onPreview);
+    this.ts.viewStack.on(Laya.Event.CLICK,this,onStackClick);
+    this.ts.tab.selectHandler = new Laya.Handler(this,onSelect);        
+    console.info(this.ts.viewStack);
+    for(var i in this.ts.viewStack._childs){
+        var item = this.ts.viewStack._childs[i];
+        for(var k in item._childs[0]._childs){
+            var image = item._childs[0]._childs[k]; //this.ts.viewStack._childs[i]._childs[0][k]
+            image.on(Laya.Event.CLICK,this,onImageClick);
+        }
+    }
+}
+
+function onPreview(){
+    console.info("preview")
+}
+
+function onStackClick(d){
+    console.info(d)
+}
+
+function onImageClick(f){
+ console.info(f,f.target instanceof Laya.Image)    
+    if(f.target instanceof Laya.Image){
+        var skin = f.target._skin;
+        // this.main.floor.skin = f.target._skin;
+         console.info(skin);
+        //  Laya.stage.addChild(this.main); 
+        var skins = skin.split("/");
+        var filename = skins[skins.length - 1];
+        skins[skins.length - 1] = "750_"+skins[skins.length - 1].substr(4);
+        var newskin = skins.join("/");
+        switch(skins[skins.length -2]){
+                case "1-floor":
+                this.main.floor.graphics.clear();
+                this.main.floor.loadImage(newskin,0,0,this.main.floor.width,this.main.floor.height);
+                break;
+                case "2-wallpaper":
+                this.main.wall.graphics.clear();
+                this.main.wall.loadImage(newskin,0,0,this.main.wall.width,this.main.wall.height);
+                break;
+                default:
+                    insertSprite(newskin);
+                break;
+        }
+        
+    }
+}
+
+function onSelect(index){
+    this.ts.viewStack.selectedIndex = index;
+}
+
+function insertSprite(skin){
+    var s = new Laya.Sprite();
+    
+    s.loadImage(skin,0,0,0,0,Laya.Handler.create(this,function(d){
+        console.info(d)
+        s.pos(this.main.wall.width/2 - d.width/2 ,this.main.wall.height/2 - d.height/2);
+        Laya.stage.addChild(s);
+        s.size(s.width,s.height);
+    }))
+    
+}
---
+++ release/web/js/ui/layaUI.max.all.js
@@ -0,0 +1,93 @@
+var CLASS$=Laya.class;
+var STATICATTR$=Laya.static;
+var View=laya.ui.View;
+var Dialog=laya.ui.Dialog;
+var mainPageUI=(function(_super){
+		function mainPageUI(){
+			
+		    this.wall=null;
+		    this.floor=null;
+
+			mainPageUI.__super.call(this);
+		}
+
+		CLASS$(mainPageUI,'ui.mainPageUI',_super);
+		var __proto__=mainPageUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(mainPageUI.uiView);
+
+		}
+
+		mainPageUI.uiView={"type":"View","props":{"width":750,"height":1334},"child":[{"type":"Sprite","props":{"y":0,"x":0,"width":750,"var":"wall","renderType":"render","height":727}},{"type":"Sprite","props":{"y":728,"x":0,"width":750,"var":"floor","renderType":"render","height":604}}]};
+		return mainPageUI;
+	})(View);
+var sharePageUI=(function(_super){
+		function sharePageUI(){
+			
+
+			sharePageUI.__super.call(this);
+		}
+
+		CLASS$(sharePageUI,'ui.sharePageUI',_super);
+		var __proto__=sharePageUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(sharePageUI.uiView);
+
+		}
+
+		sharePageUI.uiView={"type":"View","props":{"width":750,"height":280}};
+		return sharePageUI;
+	})(View);
+var startPageUI=(function(_super){
+		function startPageUI(){
+			
+		    this.percent=null;
+		    this.start=null;
+
+			startPageUI.__super.call(this);
+		}
+
+		CLASS$(startPageUI,'ui.startPageUI',_super);
+		var __proto__=startPageUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(startPageUI.uiView);
+
+		}
+
+		startPageUI.uiView={"type":"View","props":{"width":750,"height":1334},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"home/0-home/bg.png"}},{"type":"Image","props":{"y":48,"x":429,"skin":"home/0-home/logo.png"}},{"type":"Image","props":{"y":191,"x":114,"skin":"home/0-home/tle.png"}},{"type":"Image","props":{"y":377,"x":134,"skin":"home/0-home/img.png"}},{"type":"Image","props":{"y":916,"x":-1,"skin":"home/0-home/font.png"}},{"type":"Label","props":{"y":1209,"x":266,"width":160,"var":"percent","text":"内容加载中","height":25,"fontSize":16,"color":"#848e8e","align":"center"}},{"type":"Button","props":{"y":1190,"x":220,"var":"start","stateNum":1,"skin":"home/0-home/btn_but.png"}}]};
+		return startPageUI;
+	})(View);
+var tabSelectUI=(function(_super){
+		function tabSelectUI(){
+			
+		    this.tab=null;
+		    this.tabHeader=null;
+		    this.viewStack=null;
+		    this.floor=null;
+		    this.wall=null;
+		    this.sofa=null;
+		    this.cabinet=null;
+		    this.window=null;
+		    this.preview=null;
+
+			tabSelectUI.__super.call(this);
+		}
+
+		CLASS$(tabSelectUI,'ui.tabSelectUI',_super);
+		var __proto__=tabSelectUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(tabSelectUI.uiView);
+
+		}
+
+		tabSelectUI.uiView={"type":"View","props":{"y":0,"x":0,"width":750,"height":555},"child":[{"type":"Tab","props":{"y":103,"x":0,"width":750,"var":"tab","selectedIndex":0,"height":56},"child":[{"type":"Button","props":{"stateNum":2,"skin":"home/0-tab/btn_tap0.png","name":"item0"}},{"type":"Button","props":{"x":150,"stateNum":2,"skin":"home/0-tab/btn_tap1.png","name":"item1"}},{"type":"Button","props":{"x":300,"stateNum":2,"skin":"home/0-tab/btn_tap2.png","name":"item2"}},{"type":"Button","props":{"x":450,"stateNum":2,"skin":"home/0-tab/btn_tap3.png","name":"item3"}},{"type":"Button","props":{"x":600,"stateNum":2,"skin":"home/0-tab/btn_tap4.png","name":"item4"}}]},{"type":"Image","props":{"y":159,"x":0,"var":"tabHeader","skin":"home/0-tab/conbg.png","sizeGrid":"100,100,100,100"}},{"type":"ViewStack","props":{"y":159,"x":0,"var":"viewStack"},"child":[{"type":"Panel","props":{"width":750,"var":"floor","vScrollBarSkin":"comp/vscroll.png","name":"item0","height":394},"child":[{"type":"Image","props":{"y":52,"x":56,"skin":"home/1-floor/big_floor_img1.png"}},{"type":"Image","props":{"y":52,"x":314,"skin":"home/1-floor/big_floor_img2.png"}},{"type":"Image","props":{"y":52,"x":571,"skin":"home/1-floor/big_floor_img3.png"}},{"type":"Image","props":{"y":227,"x":56,"skin":"home/1-floor/big_floor_img4.png"}},{"type":"Image","props":{"y":227,"x":314,"skin":"home/1-floor/big_floor_img5.png"}},{"type":"Image","props":{"y":227,"x":571,"skin":"home/1-floor/big_floor_img6.png"}},{"type":"Image","props":{"y":401,"x":56,"skin":"home/1-floor/big_floor_img7.png"}},{"type":"Image","props":{"y":401,"x":314,"skin":"home/1-floor/big_floor_img8.png"}}]},{"type":"Panel","props":{"width":750,"var":"wall","vScrollBarSkin":"comp/vscroll.png","name":"item1","height":394},"child":[{"type":"Image","props":{"y":47,"x":53,"skin":"home/2-wallpaper/big_wall_img1.png"}},{"type":"Image","props":{"y":47,"x":314,"skin":"home/2-wallpaper/big_wall_img2.png"}},{"type":"Image","props":{"y":47,"x":572,"skin":"home/2-wallpaper/big_wall_img3.png"}},{"type":"Image","props":{"y":226,"x":53,"skin":"home/2-wallpaper/big_wall_img4.png"}},{"type":"Image","props":{"y":226,"x":312,"skin":"home/2-wallpaper/big_wall_img5.png"}},{"type":"Image","props":{"y":226,"x":570,"skin":"home/2-wallpaper/big_wall_img6.png"}},{"type":"Image","props":{"y":387,"x":53,"skin":"home/2-wallpaper/big_wall_img7.png"}}]},{"type":"Panel","props":{"width":750,"var":"sofa","vScrollBarSkin":"comp/vscroll.png","name":"item2","height":394},"child":[{"type":"Image","props":{"y":52,"x":56,"skin":"home/3-sofa/big_sofa_img1.png"}},{"type":"Image","props":{"y":56,"x":312,"skin":"home/3-sofa/big_sofa_img2.png"}},{"type":"Image","props":{"y":47,"x":573,"skin":"home/3-sofa/big_sofa_img3.png"}},{"type":"Image","props":{"y":235,"x":56,"skin":"home/3-sofa/big_sofa_img4.png"}},{"type":"Image","props":{"y":231,"x":312,"skin":"home/3-sofa/big_sofa_img5.png"}},{"type":"Image","props":{"y":228,"x":573,"skin":"home/3-sofa/big_sofa_img6.png"}},{"type":"Image","props":{"y":400,"x":58,"skin":"home/3-sofa/big_sofa_img7.png"}},{"type":"Image","props":{"y":400,"x":312,"skin":"home/3-sofa/big_sofa_img8.png"}},{"type":"Image","props":{"y":400,"x":573,"skin":"home/3-sofa/big_sofa_img9.png"}}]},{"type":"Panel","props":{"width":750,"var":"cabinet","vScrollBarSkin":"comp/vscroll.png","name":"item3","height":394},"child":[{"type":"Image","props":{"y":54,"x":55,"skin":"home/4-cabinet/big_sark_img1.png"}},{"type":"Image","props":{"y":51,"x":318,"skin":"home/4-cabinet/big_sark_img2.png"}},{"type":"Image","props":{"y":54,"x":574,"skin":"home/4-cabinet/big_sark_img3.png"}},{"type":"Image","props":{"y":227,"x":55,"skin":"home/4-cabinet/big_sark_img4.png"}},{"type":"Image","props":{"y":227,"x":318,"skin":"home/4-cabinet/big_sark_img5.png"}},{"type":"Image","props":{"y":227,"x":574,"skin":"home/4-cabinet/big_sark_img6.png"}},{"type":"Image","props":{"y":397,"x":55,"skin":"home/4-cabinet/big_sark_img7.png"}},{"type":"Image","props":{"y":397,"x":318,"skin":"home/4-cabinet/big_sark_img8.png"}},{"type":"Image","props":{"y":397,"x":574,"skin":"home/4-cabinet/big_sark_img9.png"}},{"type":"Image","props":{"y":570,"x":55,"skin":"home/4-cabinet/big_sark_img10.png"}},{"type":"Image","props":{"y":570,"x":318,"skin":"home/4-cabinet/big_sark_img11.png"}}]},{"type":"Panel","props":{"width":750,"var":"window","vScrollBarSkin":"comp/vscroll.png","name":"item4","height":394},"child":[{"type":"Image","props":{"y":57,"x":53,"skin":"home/5-window/big_window_img1.png"}},{"type":"Image","props":{"y":52,"x":314,"skin":"home/5-window/big_window_img2.png"}},{"type":"Image","props":{"y":52,"x":575,"skin":"home/5-window/big_window_img3.png"}},{"type":"Image","props":{"y":226,"x":53,"skin":"home/5-window/big_window_img4.png"}},{"type":"Image","props":{"y":226,"x":314,"skin":"home/5-window/big_window_img5.png"}},{"type":"Image","props":{"y":226,"x":575,"skin":"home/5-window/big_window_img6.png"}},{"type":"Image","props":{"y":392,"x":53,"skin":"home/5-window/big_window_img7.png"}},{"type":"Image","props":{"y":392,"x":314,"skin":"home/5-window/big_window_img8.png"}},{"type":"Image","props":{"y":392,"x":575,"skin":"home/5-window/big_window_img9.png"}},{"type":"Image","props":{"y":542,"x":53,"skin":"home/5-window/big_window_img10.png"}},{"type":"Image","props":{"y":542,"x":314,"skin":"home/5-window/big_window_img11.png"}},{"type":"Image","props":{"y":542,"x":575,"skin":"home/5-window/big_window_img12.png"}}]}]},{"type":"Button","props":{"y":-2,"x":632,"var":"preview","stateNum":1,"skin":"home/top/photo_icon.png"}}]};
+		return tabSelectUI;
+	})(View);
